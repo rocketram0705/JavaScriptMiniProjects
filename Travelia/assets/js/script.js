2c85bce9751464ef926e9a4dd86a63d7d229868c
rocketram0705 <sharoraj2710@gmail.com>
2023-02-15 23:10:14 +0530

    adder header html,css,js

--- Travelia/assets/js/script.js
+++ Travelia/assets/js/script.js
@@ -2,3 +2,28 @@ let preloader = document.querySelector("[data-preloader]");
 window.addEventListener('load',()=>{
     preloader.classList.add("remove");
 });
+
+/* add event on multiple elements */
+const addEventsOnElements = function (elements,eventType,callback){
+    for(let i=0;i<elements.length;i++){
+        elements[i].addEventListener(eventType,callback);
+    }
+};
+/* nav bar toggler for mobile */
+const navbar = document.querySelector("[data-navbar]");
+const navtogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+const toggleNav = function (){
+    navbar.classList.toggle('active');
+    //navtogglers.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('nav-active');
+}
+addEventsOnElements(navtogglers,"click",toggleNav);
+
+/* header while navigating */
+const header = document.querySelector("[data-header]");
+
+window.addEventListener('scroll',function(){
+    header.classList[window.scrollY > 100 ?"add" : "remove"]("active");
+});
